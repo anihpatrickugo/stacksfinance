332705209b8fa890a55dd5a4f3e2e5ad53cabecd
patrick <iampatrickugo@gmail.com>
2022-08-13 11:16:55 +0100

    updated settings

--- stacksfinance/settings.py
+++ stacksfinance/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l+cs9_y#h449-#acu6b1rh4s9j8^xms%-$0h!g3sy%o0ut@p35
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stacksfinance.herokuapp.com']
 
 
 # Application definition
